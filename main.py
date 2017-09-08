fbb177af252c202fbf674e44e959a750d40bce3b
Eliabe Puchalski <eliabe@alunos.utfpr.edu.br>
2017-09-08 18:41:02 -0300

    init

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import telepot
 
 telegram = telepot.Bot("262863240:AAHreV8hDp_EYAbbmGpcrPCP8_2VJ6HPJtc")
 
-sons = ('play sons', 'play soa', 'play sons of anarchy', 'play sonsofanarchy')
-shameless = ('play shameless', 'play shameless us')
+twobrokegirls = ('2bg', '2 broke girls')
+narcos = ('narcos')
+shameless = ('shameless', 'shameless us')
+sons = ('sons', 'soa', 'sons of anarchy', 'sonsofanarchy')
+
 def recebendoMsg(msg):
     frase = msg['text']
     frase = frase.lower()
     tipoMsg, tipoChat, chatID = telepot.glance(msg)
 
-    if frase in sons:
-        resp = 'CAADAQAD3goAAuZdXQE8WyJzDTSomAI'
-    if frase in shameless:
-        resp = 'CAADAQADZgoAAuZdXQG-CK4aW3AfOQI'
-    
-    telegram.sendSticker(chatID,resp)
-
+    if '/play ' in frase:
+        nomedaserie = frase[6:]
+        if nomedaserie in sons:
+            resp = 'playsonsofanarchy'
+        elif nomedaserie in shameless:
+            resp = 'CAADAgADxAUAAvoLtgiCOopVZ7H8dwI'
+        elif nomedaserie in narcos:
+            resp = 'CAADAQADRQoAAuZdXQGWXScs1RuLMAI'
+        elif nomedaserie in twobrokegirls:
+            resp = 'CAADAQADOgsAAuZdXQGb1rpQJ2A4YwI'
+        else:
+            resp = 'não entendi'
+        telegram.sendSticker(chatID,resp)
+      
 telegram.message_loop(recebendoMsg)
 
 while True:
